e9d551dd5a771bb5eae327226d8025857c537079
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-03-03 09:33:44 +0200

    Add files via upload

---
+++ JavaCore 1217 Fly, run and swim/src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+/*
+
+1217 fly, run and swim
+Write the public interfaces Fly (fly), Run (run / ride), Swim (swim).
+Add one method to each interface.
+
+Requirements:
+1. The Solution class must contain the Fly interface.
+2. The Solution class must contain the Run interface.
+3. The Solution class must contain the Swim interface.
+4. The Fly interface must contain one method.
+5. The Run interface must contain one method.
+6. The Swim interface must contain one method.
+
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public interface Fly {
+        public void start();
+    }
+
+    public interface Run {
+        public void start();
+    }
+
+    public interface Swim {
+        public void start();
+    }
+}
